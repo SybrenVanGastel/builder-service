aa1fdfb2cb1512b90c293e0d39aa56285b200b45
SybrenVanGastel <r0785524@student.thomasmore.be>
2021-10-15 11:18:29 +0200

    initial commit

---
+++ builder-service/src/main/java/be/itf/builderservice/BuilderServiceApplication.java
@@ -0,0 +1,13 @@
+package be.itf.builderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuilderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuilderServiceApplication.class, args);
+	}
+
+}
---
+++ builder-service/src/test/java/be/itf/builderservice/BuilderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package be.itf.builderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuilderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
